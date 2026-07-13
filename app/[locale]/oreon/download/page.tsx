2029cca4963e2f17ec29320b1c13a095b539dbab
Brandon Lester <boostyconnect@oreonproject.org>
2026-07-13 02:56:16 -0700

    feat: or10 2607
    oreon 10 build 2607

--- app/[locale]/oreon/download/page.tsx
+++ app/[locale]/oreon/download/page.tsx
@@ -74,14 +74,14 @@ export default function Download() {
     {
       nameKey: 'download.oreon10' as const,
       taglineKey: 'download.latestVersion' as const,
-      build: '2606',
+      build: '2607',
       descKey: 'download.oreon10Desc' as const,
       sourceLink: 'https://copr.fedorainfracloud.org/coprs/brandonlester/oreon-10/',
       statusKey: 'download.latestRelease' as const,
       color: 'text-[#007b56]',
       image: '/oreon10.png',
       architectures: [
-        { label: 'x86_64', noteKey: 'download.intelAmd' as const, hash: '11e6943f4387861d58e196818043d8b9efe006b9290327ab17086db807ba4a77', link: 'https://mirrors.oreonhq.com/Oreon-10-2606-x86_64.iso' },
+        { label: 'x86_64', noteKey: 'download.intelAmd' as const, hash: 'd3c586be9b5d832bd3e6cb1f3c4ca4ab96a500889492204c0e8e4ab4cd108395', link: 'https://mirrors.oreonhq.com/Oreon-10-2607-x86_64.iso' },
         { label: 'aarch64', noteKey: 'download.arm64' as const, hash: 'c0629d741b3ac0c72667ea5dac9e4a38648047c854716302edee394b18600ec8', link: 'https://mirrors.oreonhq.com/Oreon-10-2606-aarch64.iso' },
       ],
     },
